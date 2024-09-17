d5c10e7a89df5d88c5e778396d93fb249ec4b93e
SGiaccobasso <sgiaccobasso@gmail.com>
2024-09-17 14:16:11 -0300

    separate functions

--- script.ts
+++ script.ts
@@ -31,7 +31,9 @@ interface TokenInfo {
   chains: ChainInfo[];
 }
 type TokenManagerType = (typeof tokenManagerTypes)[number];
+
 // Constants
+
 const tokenManagerTypes = [
   "nativeInterchainToken",
   "mintBurnFrom",
@@ -41,6 +43,7 @@ const tokenManagerTypes = [
   "gateway",
 ] as const;
 const ITSAddress = "0xB5FB4BE02232B1bBA4dC8f81dc24C26980dE9e3C";
+const COINGECKO_API_KEY = "CG-3VGxh1K3Qk7jAvpt4DJA3LvB";
 const CHAIN_CONFIGS_URL =
   "https://axelar-mainnet.s3.us-east-2.amazonaws.com/configs/mainnet-config-1.x.json";
 const ERC20ABI = [
@@ -112,8 +115,22 @@ async function getAxelarChains() {
 }
 
 async function getRpcUrl(axelarChainId: string): Promise<string | null> {
-  const chains = await getAxelarChains();
-  return chains[axelarChainId].config.rpc[0];
+  try {
+    const chains = await getAxelarChains();
+    return chains[axelarChainId].config.rpc[0];
+  } catch (error) {
+    throw new Error(
+      `Error fetching chain configs for chain '${axelarChainId}': ${
+        (error as Error).message
+      }`
+    );
+  }
+}
+
+function exitWitheError(errorMessage: string) {
+  console.error(errorMessage);
+  fs.writeFileSync("validation_errors.txt", errorMessage);
+  process.exit(1);
 }
 
 /*
@@ -121,7 +138,6 @@ async function getRpcUrl(axelarChainId: string): Promise<string | null> {
  * Section: Main Functions
  * =============================
  */
-
 async function validateTokenInfo(
   tokenInfo: Record<string, TokenInfo>
 ): Promise<string[]> {
@@ -129,248 +145,228 @@ async function validateTokenInfo(
 
   for (const [tokenId, info] of Object.entries(tokenInfo)) {
     console.log(`\nValidating token: ${tokenId}`);
-
-    // Validate token ID
-    if (tokenId !== info.tokenId)
-      errors.push(`Mismatch in tokenId: ${tokenId} vs ${info.tokenId}`);
-
-    // Validate CoinGecko ID with CoinGecko API
-    if (info.coinGeckoId) {
-      try {
-        const response = await axios.get(
-          `https://api.coingecko.com/api/v3/coins/${info.coinGeckoId}`,
-          {
-            headers: {
-              "x-cg-demo-api-key": "CG-3VGxh1K3Qk7jAvpt4DJA3LvB",
-            },
-          }
-        );
-
-        if (response.status === 200) {
-          const coinData = response.data;
-
-          // Validate symbol
-          if (
-            coinData.symbol.toLowerCase() !== info.prettySymbol.toLowerCase()
-          ) {
-            errors.push(
-              `CoinGecko symbol (${coinData.symbol}) does not match prettySymbol (${info.prettySymbol}) for token ${tokenId}`
-            );
-          }
-        } else {
-          errors.push(
-            `CoinGecko API returned status ${response.status} for token ${tokenId}`
-          );
-        }
-      } catch (error) {
-        if (axios.isAxiosError(error) && error.response?.status === 404) {
-          errors.push(
-            `CoinGecko ID ${info.coinGeckoId} not found for token ${tokenId}`
-          );
-        } else {
-          errors.push(
-            `Error fetching data from CoinGecko for token ${tokenId}: ${
-              (error as Error).message
-            }`
-          );
-        }
-      }
-    } else {
-      errors.push(`CoinGecko ID is missing for token ${tokenId}`);
-    }
-
-    // Validate deployer address
-    if (!ethers.isAddress(info.deployer))
-      errors.push(`Invalid deployer address: ${info.deployer}`);
-
-    // Validate chains
-    for (const chain of info.chains) {
-      console.log(`Validating chain: ${chain.axelarChainId}`);
-
-      // Create provider
-      const rpcUrl = await getRpcUrl(chain.axelarChainId);
-      if (!rpcUrl) {
-        errors.push(`No RPC URL found for chain ${chain.axelarChainId}`);
-        continue;
-      }
-      const provider = new ethers.JsonRpcProvider(rpcUrl);
-
-      // Validate token address
-      const tokenCode = await provider.getCode(chain.tokenAddress);
-      if (tokenCode === "0x") {
-        errors.push(
-          `Token address ${chain.tokenAddress} does not exist on chain ${chain.axelarChainId}`
-        );
-      } else {
-        // Validate token details
-        const tokenContract = new ethers.Contract(
-          chain.tokenAddress,
-          ERC20ABI,
-          provider
-        );
-        try {
-          const tokenName = await tokenContract.name();
-          const tokenSymbol = await tokenContract.symbol();
-          const tokenDecimals = await tokenContract.decimals();
-          const decimalsFromContract = Number(tokenDecimals);
-
-          if (tokenName.toLowerCase() !== chain.name.toLowerCase()) {
-            errors.push(
-              `Token name mismatch on chain ${chain.axelarChainId}: expected ${chain.name}, got ${tokenName}`
-            );
-          }
-          if (tokenSymbol.toLowerCase() !== chain.symbol.toLowerCase()) {
-            errors.push(
-              `Token symbol mismatch on chain ${chain.axelarChainId}: expected ${chain.symbol}, got ${tokenSymbol}`
-            );
-          }
-          // If it's original chain, compare with parent object
-          if (info.originAxelarChainId === chain.axelarChainId) {
-            if (tokenSymbol.toLowerCase() !== info.prettySymbol.toLowerCase()) {
-              errors.push(
-                `Token symbol mismatch on chain ${chain.axelarChainId}: expected ${info.prettySymbol}, got ${tokenSymbol}`
-              );
-            }
-            if (decimalsFromContract !== info.decimals) {
-              errors.push(
-                `Token decimals mismatch on chain ${chain.axelarChainId}: expected ${info.decimals}, got ${decimalsFromContract}`
-              );
-            }
-          }
-        } catch (error) {
-          errors.push(
-            `Error fetching token info on chain ${chain.axelarChainId}: ${
-              (error as Error).message
-            }`
-          );
-        }
-      }
-
-      // Validate token manager
-      const managerCode = await provider.getCode(chain.tokenManager);
-      if (managerCode === "0x") {
-        errors.push(
-          `Token manager ${chain.tokenManager} does not exist on chain ${chain.axelarChainId}`
-        );
-      } else {
-        const tokenManagerContract = new ethers.Contract(
-          chain.tokenManager,
-          tokenManagerABI,
-          provider
-        );
-        try {
-          // Validate token manager address
-          const managedTokenAddress = await tokenManagerContract.tokenAddress();
-          if (
-            managedTokenAddress.toLowerCase() !==
-            chain.tokenAddress.toLowerCase()
-          ) {
-            errors.push(
-              `Token manager ${chain.tokenManager} on chain ${chain.axelarChainId} does not manage the specified token address ${chain.tokenAddress}`
-            );
-          }
-          // Validate token manager type
-          const implementationType =
-            await tokenManagerContract.implementationType();
-          if (
-            Number(implementationType) !==
-            tokenManagerTypes.indexOf(chain.tokenManagerType)
-          ) {
-            errors.push(
-              `Token manager on chain ${chain.axelarChainId} has incorrect implementation type: ` +
-                `expected ${
-                  tokenManagerTypes[Number(implementationType)] || "Unknown"
-                }, ` +
-                `got ${chain.tokenManagerType}`
-            );
-          }
-        } catch (error) {
-          errors.push(
-            `Error verifying token manager on chain ${chain.axelarChainId}: ${
-              (error as Error).message
-            }`
-          );
-        }
-      }
-    }
-
-    // Validate origin chain
-    const originChain = info.chains.find(
-      (chain) => chain.axelarChainId === info.originAxelarChainId
-    );
-    if (!originChain) {
-      errors.push(
-        `Origin chain ${info.originAxelarChainId} not found in chains list`
-      );
-    }
-
-    // Validate interchainTokenId with deployer and salt
-    if (originChain) {
-      const rpcUrl = await getRpcUrl(info.originAxelarChainId);
-      if (!rpcUrl) {
-        errors.push(
-          `No RPC URL found for origin chain ${info.originAxelarChainId}`
-        );
-        continue;
-      }
-      const provider = new ethers.JsonRpcProvider(rpcUrl);
-      const itsContract = new ethers.Contract(ITSAddress, ITSABI, provider);
-
-      try {
-        const calculatedTokenId = await itsContract.interchainTokenId(
-          info.deployer,
-          info.deploySalt
-        );
-
-        if (calculatedTokenId.toLowerCase() !== tokenId.toLowerCase()) {
-          errors.push(
-            `Mismatch in interchainTokenId for token ${tokenId}:\n` +
-              `  Expected:   ${tokenId}\n` +
-              `  Calculated: ${calculatedTokenId}\n` +
-              `  Deployer:   ${info.deployer}\n` +
-              `  Deploy Salt: ${info.deploySalt}`
-          );
-        }
-      } catch (error) {
-        errors.push(
-          `Error calculating interchainTokenId for token ${tokenId}: ${
-            (error as Error).message
-          }`
-        );
-      }
+    try {
+      await validateTokenId(tokenId, info);
+      await validateCoinGeckoId(tokenId, info);
+      await validateDeployerAddress(info);
+      await validateChains(tokenId, info);
+      await validateOriginChain(info);
+      await validateInterchainTokenId(tokenId, info);
+    } catch (error) {
+      exitWitheError((error as Error).message);
     }
   }
-
   return errors;
 }
 
-async function main() {
-  const newTokens: Record<string, unknown> = JSON.parse(
-    fs.readFileSync("./new_tokens.json", "utf8")
-  );
-
-  try {
-    const validationErrors = await validateTokenInfo(
-      newTokens as Record<string, TokenInfo>
-    );
-    if (validationErrors.length > 0) {
-      console.log("Validation errors found:", validationErrors.length);
-      for (const error of validationErrors) {
-        console.error(error);
-      }
-      fs.writeFileSync("validation_errors.txt", validationErrors.join("\n"));
-      process.exit(1);
-    } else {
-      console.log("All new token configurations are valid.");
-    }
-  } catch (error) {
-    console.error("An error occurred during validation:", error);
-    fs.writeFileSync("validation_errors.txt", (error as Error).toString());
-    process.exit(1);
+async function validateTokenId(
+  tokenId: string,
+  info: TokenInfo
+): Promise<void> {
+  if (tokenId !== info.tokenId) {
+    throw new Error(`Mismatch in tokenId: ${tokenId} vs ${info.tokenId}`);
   }
 }
 
-main().catch((error) => {
-  console.error("Unhandled error in main function:", error);
-  process.exit(1);
-});
+async function validateCoinGeckoId(
+  tokenId: string,
+  info: TokenInfo
+): Promise<void> {
+  if (!info.coinGeckoId) {
+    throw new Error(`CoinGecko ID is missing for token ${tokenId}`);
+  }
+
+  try {
+    const response = await axios.get(
+      `https://api.coingecko.com/api/v3/coins/${info.coinGeckoId}`,
+      {
+        headers: {
+          "x-cg-demo-api-key": COINGECKO_API_KEY,
+        },
+      }
+    );
+
+    if (response.status !== 200)
+      throw new Error(
+        `CoinGecko API returned status ${response.status} for token ${tokenId}`
+      );
+
+    const coinData = response.data;
+    if (coinData.symbol.toLowerCase() !== info.prettySymbol.toLowerCase()) {
+      throw new Error(
+        `CoinGecko symbol (${coinData.symbol}) does not match prettySymbol (${info.prettySymbol}) for token ${tokenId}`
+      );
+    }
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response?.status === 404) {
+      throw new Error(
+        `CoinGecko ID ${info.coinGeckoId} not found for token ${tokenId}`
+      );
+    }
+    throw new Error(
+      `Error fetching data from CoinGecko for token ${tokenId}: ${
+        (error as Error).message
+      }`
+    );
+  }
+}
+
+async function validateDeployerAddress(info: TokenInfo): Promise<void> {
+  if (!ethers.isAddress(info.deployer)) {
+    throw new Error(`Invalid deployer address: ${info.deployer}`);
+  }
+}
+
+async function validateChains(tokenId: string, info: TokenInfo): Promise<void> {
+  for (const chain of info.chains) {
+    console.log(`Validating chain: ${chain.axelarChainId}`);
+
+    const rpcUrl = await getRpcUrl(chain.axelarChainId);
+    if (!rpcUrl) {
+      throw new Error(`No RPC URL found for chain ${chain.axelarChainId}`);
+    }
+
+    const provider = new ethers.JsonRpcProvider(rpcUrl);
+
+    await validateTokenAddress(chain, provider);
+    await validateTokenDetails(chain, info, provider);
+    await validateTokenManager(chain, provider);
+  }
+}
+
+async function validateTokenAddress(
+  chain: any,
+  provider: ethers.JsonRpcProvider
+): Promise<void> {
+  const tokenCode = await provider.getCode(chain.tokenAddress);
+  if (tokenCode === "0x")
+    throw new Error(
+      `Token address ${chain.tokenAddress} does not exist on chain ${chain.axelarChainId}`
+    );
+}
+
+async function validateTokenDetails(
+  chain: any,
+  info: TokenInfo,
+  provider: ethers.JsonRpcProvider
+): Promise<void> {
+  const tokenContract = new ethers.Contract(
+    chain.tokenAddress,
+    ERC20ABI,
+    provider
+  );
+  const tokenName = await tokenContract.name();
+  const tokenSymbol = await tokenContract.symbol();
+  const tokenDecimals = await tokenContract.decimals();
+  const decimalsFromContract = Number(tokenDecimals);
+
+  if (tokenName.toLowerCase() !== chain.name.toLowerCase())
+    throw new Error(
+      `Token name mismatch on chain ${chain.axelarChainId}: expected ${chain.name}, got ${tokenName}`
+    );
+  if (tokenSymbol.toLowerCase() !== chain.symbol.toLowerCase())
+    throw new Error(
+      `Token symbol mismatch on chain ${chain.axelarChainId}: expected ${chain.symbol}, got ${tokenSymbol}`
+    );
+  if (info.originAxelarChainId === chain.axelarChainId) {
+    if (tokenSymbol.toLowerCase() !== info.prettySymbol.toLowerCase())
+      throw new Error(
+        `Token symbol mismatch on chain ${chain.axelarChainId}: expected ${info.prettySymbol}, got ${tokenSymbol}`
+      );
+    if (decimalsFromContract !== info.decimals)
+      throw new Error(
+        `Token decimals mismatch on chain ${chain.axelarChainId}: expected ${info.decimals}, got ${decimalsFromContract}`
+      );
+  }
+}
+
+async function validateTokenManager(
+  chain: any,
+  provider: ethers.JsonRpcProvider
+): Promise<void> {
+  const managerCode = await provider.getCode(chain.tokenManager);
+  if (managerCode === "0x") {
+    throw new Error(
+      `Token manager ${chain.tokenManager} does not exist on chain ${chain.axelarChainId}`
+    );
+  }
+
+  const tokenManagerContract = new ethers.Contract(
+    chain.tokenManager,
+    tokenManagerABI,
+    provider
+  );
+  const managedTokenAddress = await tokenManagerContract.tokenAddress();
+  if (managedTokenAddress.toLowerCase() !== chain.tokenAddress.toLowerCase())
+    throw new Error(
+      `Token manager ${chain.tokenManager} on chain ${chain.axelarChainId} does not manage the specified token address ${chain.tokenAddress}`
+    );
+
+  const implementationType = await tokenManagerContract.implementationType();
+  if (
+    Number(implementationType) !==
+    tokenManagerTypes.indexOf(chain.tokenManagerType)
+  )
+    throw new Error(
+      `Token manager on chain ${
+        chain.axelarChainId
+      } has incorrect implementation type: expected '${
+        tokenManagerTypes[Number(implementationType)] || "Unknown"
+      }', got '${chain.tokenManagerType}'`
+    );
+}
+
+async function validateOriginChain(info: TokenInfo): Promise<void> {
+  const originChain = info.chains.find(
+    (chain) => chain.axelarChainId === info.originAxelarChainId
+  );
+  if (!originChain) {
+    throw new Error(
+      `Origin chain ${info.originAxelarChainId} not found in chains list`
+    );
+  }
+}
+
+async function validateInterchainTokenId(
+  tokenId: string,
+  info: TokenInfo
+): Promise<void> {
+  const rpcUrl = await getRpcUrl(info.originAxelarChainId);
+  if (!rpcUrl) {
+    throw new Error(
+      `No RPC URL found for origin chain ${info.originAxelarChainId}`
+    );
+  }
+
+  const provider = new ethers.JsonRpcProvider(rpcUrl);
+  const itsContract = new ethers.Contract(ITSAddress, ITSABI, provider);
+
+  const calculatedTokenId = await itsContract.interchainTokenId(
+    info.deployer,
+    info.deploySalt
+  );
+
+  if (calculatedTokenId.toLowerCase() !== tokenId.toLowerCase())
+    throw new Error(
+      `Mismatch in interchainTokenId for token ${tokenId}:\n` +
+        `Deployer or Deploy Salt could be incorrect\n` +
+        `  Expected:   ${tokenId}\n` +
+        `  Calculated: ${calculatedTokenId}\n` +
+        `  Deployer:   ${info.deployer}\n` +
+        `  Deploy Salt: ${info.deploySalt}`
+    );
+}
+
+async function main() {
+  try {
+    // Read new token configurations from file
+    const newTokens: Record<string, TokenInfo> = JSON.parse(
+      fs.readFileSync("./new_tokens.json", "utf8")
+    );
+    await validateTokenInfo(newTokens);
+  } catch (error) {
+    exitWitheError((error as Error).message);
+  }
+}
+
+main();
